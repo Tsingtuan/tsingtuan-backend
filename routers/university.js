1d72e8be28927ebb7c7e6d8ab05ea43eefd02673
zsha2 <shazjgeorge@gmail.com>
2018-09-16 21:30:58 -0400

    added get method to university router

--- routers/university.js
+++ routers/university.js
@@ -1,7 +1,11 @@
 var pool = require("../datebase-pool.js");
 
 var universityCollection = {
-
+  getAll: async function(req, res) {
+    let query = "SELECT * FROM university_table";
+    let result = await pool.query(query);
+    return result;
+  }
 };
 
 module.exports = universityCollection;
